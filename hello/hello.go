e880ede3b7b252b21fd4a2ccbd0317d3d7e46d8f
jeff-vincent <jeff.d.vincent@gmail.com>
2022-05-12 17:00:58 -0700

    First steps with Golang

---
+++ hello/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	names := []string{"Bill", "Toby", "Phil"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
